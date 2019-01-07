aede7a803d5d179589ce1d69f52c4a37a16a9b2e
ttyrion <tyrion.marting@hotmail.com>
2019-01-07 15:50:32 +0800

    try-catch 后的 finally代码块：如果异常没有被catch到

--- chapter1/src/test/java/soundsystem/CDPlayerTest.java
+++ chapter1/src/test/java/soundsystem/CDPlayerTest.java
@@ -11,12 +11,30 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
+    private int i = 0;
     @Autowired
     private CDPlayer player = null;
 
     @Test
+    public void onTest() {
+        while(i <= 100) {
+            ++i;
+            System.out.println("onTest times: " + i);
+            try{
+                play();
+            }
+            catch(NullPointerException e) {
+                System.out.println("onTest RuntimeException catched.");
+            }
+            finally {
+                System.out.println("onTest RuntimeException out of catch.");
+            }
+        }
+    }
+    
     public void play() {
         System.out.println("Test running...");
         player.play();
+        throw new RuntimeException();
     }
 }
